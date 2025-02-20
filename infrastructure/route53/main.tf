e4ed1b2e1c8a9f054f7d974a9935c03e5873baa5
LasseNybro <lasso290391@hotmail.com>
2025-02-21 00:37:19 +0100

    added www. route resolver as well

--- infrastructure/route53/main.tf
+++ infrastructure/route53/main.tf
@@ -1,9 +1,9 @@
-resource "aws_route53_zone" "route_hosted_zone" {
+resource "aws_route53_zone" "route_hosted_zone_root" {
   name = var.domain_name
 }
 
 resource "aws_route53_record" "route_record" {
-  zone_id = aws_route53_zone.route_hosted_zone.zone_id
+  zone_id = aws_route53_zone.route_hosted_zone_root.zone_id
   name    = var.domain_name
   type    = "A"
   alias {
@@ -12,3 +12,33 @@ resource "aws_route53_record" "route_record" {
     evaluate_target_health = true
   }
 }
+
+resource "aws_route53_zone" "route_hosted_zone_www" {
+  name = "www.${var.domain_name}"
+}
+
+resource "aws_route53_record" "route_record" {
+  zone_id = aws_route53_zone.route_hosted_zone_www.zone_id
+  name    = var.domain_name
+  type    = "A"
+  alias {
+    name                   = var.s3_bucket_website_endpoint
+    zone_id                = var.s3_bucket_hosted_zone_id
+    evaluate_target_health = true
+  }
+}
+
+/*resource "aws_route53_zone" "route_hosted_zone" {
+  name = "api.${var.domain_name}"
+}
+
+resource "aws_route53_record" "route_record" {
+  zone_id = aws_route53_zone.route_hosted_zone.zone_id
+  name    = var.domain_name
+  type    = "A"
+  alias {
+    name                   = var.
+    zone_id                = var.s3_bucket_hosted_zone_id
+    evaluate_target_health = true
+  }
+}*/
