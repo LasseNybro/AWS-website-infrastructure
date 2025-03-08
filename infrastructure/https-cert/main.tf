6033b6f4fdac87e5748e9d1312170595de53cdee
LasseNybro <lasso290391@hotmail.com>
2025-03-08 21:47:48 +0100

    rtest

--- infrastructure/https-cert/main.tf
+++ infrastructure/https-cert/main.tf
@@ -1,5 +1,4 @@
 resource "aws_acm_certificate" "https_cert" {
-  provider                 = aws.us_east_1
   domain_name               = var.domain_name
   subject_alternative_names = ["www.${var.domain_name}"]
   validation_method         = "DNS"
